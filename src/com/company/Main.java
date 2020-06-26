5c6cdf0b054e68f836c7dc29dafb34d4342431ba
ndwyer5719 <67170639+ndwyer5719@users.noreply.github.com>
2020-06-25 20:03:19 -0400

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,59 @@
 package com.company;
+import java.util.ArrayList;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-    }
+
+      Vehicle v1 = new Vehicle("12124324");
+      v1.setColor(Color.Orange);
+      v1.setMake(Make.Chevy);
+      Vehicle v2 = new Vehicle("12132423");
+      v2.setColor(Color.Green);
+      v2.setMake(Make.Ford);
+      Vehicle v3 = new Vehicle("12697976");
+      v3.setColor(Color.Blue);
+      v3.setMake(Make.RAM);
+
+      ArrayList<Vehicle> vehicleList = new ArrayList<>();
+      vehicleList.add(v1);
+      vehicleList.add(v2);
+      vehicleList.add(v3);
+
+      String vehicleId = "12697976";
+
+      //Search string by vehicleId
+      for (Vehicle v : vehicleList) {
+         if (vehicleId == v.getId()) {
+            System.out.println(v.getColor());
+            break;
+         }
+      }
+
+      //Update Search string by vehicleId set Color
+      for (Vehicle v : vehicleList) {
+         if (vehicleId == v.getId()) {
+            v.setColor(Color.Purple);
+            break;
+         }
+      }
+
+      //Print all vehicle objects in ArrayList
+      for (Vehicle v : vehicleList) {
+            System.out.println("Id: " + v.getId() + " Make: " + v.getMake());
+      }
+
+      //Remove Search string by vehicleId set Color
+      for (Vehicle v : vehicleList) {
+         if (vehicleId == v.getId()) {
+            vehicleList.remove(v);
+            break;
+         }
+      }
+
+      //Print all vehicle objects in ArrayList
+      for (Vehicle v : vehicleList) {
+         System.out.println("Id: " + v.getId() + " Make: " + v.getMake());
+      }
+   }
 }
